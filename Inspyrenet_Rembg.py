fcb5d5ac35f062beb9533584636e23b8a804ea2e
Yao Chi <later@usopp.net>
2024-07-22 22:49:12 +0800

    refine the nodes

--- Inspyrenet_Rembg.py
+++ Inspyrenet_Rembg.py
@@ -7,70 +7,91 @@ from tqdm import tqdm
 
 # Tensor to PIL
 def tensor2pil(image):
-    return Image.fromarray(np.clip(255. * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
+    return Image.fromarray(
+        np.clip(255.0 * image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8)
+    )
+
 
 # Convert PIL to Tensor
 def pil2tensor(image):
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
-class InspyrenetRembg:
-    def __init__(self):
-        pass
-    
+
+class InspyrenetRemover:
     @classmethod
     def INPUT_TYPES(s):
         return {
-            "required": {
-                "image": ("IMAGE",),
-                "torchscript_jit": (["default", "on"],)
-            },
+            "required": {"torchscript_jit": (["default", "on"],)},
+        }
+
+    RETURN_TYPES = ("REMOVER",)
+    FUNCTION = "init_remover"
+    CATEGORY = "InspyreNet"
+
+    def init_remover(self, torchscript_jit):
+        if torchscript_jit == "default":
+            remover = Remover(
+                ckpt="/workspace/transparent_background/ckpt_base.pth"
+            )
+        else:
+            remover = Remover(
+                jit=True,
+                ckpt="/workspace/transparent_background/ckpt_base.pth",
+            )
+        return (remover,)
+
+
+class InspyrenetRembg:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {"remover": ("REMOVER",), "image": ("IMAGE",),},
         }
 
     RETURN_TYPES = ("IMAGE", "MASK")
     FUNCTION = "remove_background"
-    CATEGORY = "image"
+    CATEGORY = "InspyreNet"
 
-    def remove_background(self, image, torchscript_jit):
-        if (torchscript_jit == "default"):
-            remover = Remover()
-        else:
-            remover = Remover(jit=True)
+    def remove_background(self, remover, image):
         img_list = []
         for img in tqdm(image, "Inspyrenet Rembg"):
-            mid = remover.process(tensor2pil(img), type='rgba')
-            out =  pil2tensor(mid)
+            mid = remover.process(tensor2pil(img), type="rgba")
+            out = pil2tensor(mid)
             img_list.append(out)
         img_stack = torch.cat(img_list, dim=0)
         mask = img_stack[:, :, :, 3]
         return (img_stack, mask)
-        
+
+
 class InspyrenetRembgAdvanced:
     def __init__(self):
         pass
-    
+
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
+                "remover": ("REMOVER",),
                 "image": ("IMAGE",),
-                "threshold": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01}),
-                "torchscript_jit": (["default", "on"],)
+                "threshold": (
+                    "FLOAT",
+                    {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01},
+                ),
             },
         }
 
     RETURN_TYPES = ("IMAGE", "MASK")
     FUNCTION = "remove_background"
-    CATEGORY = "image"
+    CATEGORY = "InspyreNet"
 
-    def remove_background(self, image, torchscript_jit, threshold):
-        if (torchscript_jit == "default"):
-            remover = Remover()
-        else:
-            remover = Remover(jit=True)
+    def remove_background(self, remover, image, threshold):
         img_list = []
         for img in tqdm(image, "Inspyrenet Rembg"):
-            mid = remover.process(tensor2pil(img), type='rgba', threshold=threshold)
-            out =  pil2tensor(mid)
+            mid = remover.process(tensor2pil(img), type="rgba", threshold=threshold)
+            out = pil2tensor(mid)
             img_list.append(out)
         img_stack = torch.cat(img_list, dim=0)
         mask = img_stack[:, :, :, 3]
--- __init__.py
+++ __init__.py
@@ -1,12 +1,18 @@
-from .Inspyrenet_Rembg import InspyrenetRembg, InspyrenetRembgAdvanced
+from .Inspyrenet_Rembg import (
+    InspyrenetRembg,
+    InspyrenetRembgAdvanced,
+    InspyrenetRemover,
+)
 
 NODE_CLASS_MAPPINGS = {
-    "InspyrenetRembg" : InspyrenetRembg,
-    "InspyrenetRembgAdvanced" : InspyrenetRembgAdvanced,
+    "InspyrenetRemover": InspyrenetRemover,
+    "InspyrenetRembg": InspyrenetRembg,
+    "InspyrenetRembgAdvanced": InspyrenetRembgAdvanced,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "InspyrenetRemover": "Inspyrenet Remover",
     "InspyrenetRembg": "Inspyrenet Rembg",
-    "InspyrenetRembgAdvanced": "Inspyrenet Rembg Advanced"
+    "InspyrenetRembgAdvanced": "Inspyrenet Rembg Advanced",
 }
-__all__ = ['NODE_CLASS_MAPPINGS', "NODE_DISPLAY_NAME_MAPPINGS"]
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
